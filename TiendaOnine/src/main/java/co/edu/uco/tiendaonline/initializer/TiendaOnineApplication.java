cc3ed6b9fea75e0d64180991b37ee21703475569
Sergio <=sergioarias1001@gmail.com>
2023-10-25 19:35:31 -0500

    sesion 15-2

--- TiendaOnine/src/main/java/co/edu/uco/tiendaonline/initializer/TiendaOnineApplication.java
+++ TiendaOnine/src/main/java/co/edu/uco/tiendaonline/initializer/TiendaOnineApplication.java
@@ -2,8 +2,10 @@ package co.edu.uco.tiendaonline.initializer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan (basePackages="co.edu.uco.tiendaonline.controller")
 public class TiendaOnineApplication {
 
 	public static void main(String[] args) {
